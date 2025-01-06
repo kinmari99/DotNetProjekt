eba652df7855e5197ac47e474bd63ce18d4201cb
kinmari99 <kinga.wojtanowicz@microsoft.wsei.edu.pl>
2025-01-06 17:46:49 +0100

    Dodaj pliki projektów.

---
+++ Hotel/Controllers/NewsletterController.cs
@@ -0,0 +1,27 @@
+﻿using Hotel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Controllers
+{
+    public class NewsletterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Newsletter(Newsletter newsletter)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Wynik", newsletter);
+            }
+            else { return View("Index", newsletter); }
+        }
+        [HttpPost]
+        public IActionResult Wynik(Newsletter newsletter)
+        {
+            return View(newsletter);
+        }
+    }
+}
---
+++ Hotel/Models/Newsletter.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hotel.Models
+{
+    public class Newsletter
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Proszę podaj Imię")]
+        [MinLength(2)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proszę podaj Email")]
+        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
+        public string Mail {  get; set; }
+
+        [Display(Name = "Wyrażam zgodę na przetwarzanie moich danych przez Hotel XYZ")]
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Musisz wyrazić zgodę.")]
+        public bool IsAccepted { get; set; }
+    }
+}
