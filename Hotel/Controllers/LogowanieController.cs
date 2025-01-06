ec32a25c7021918050907d7d3ab42381f3413fba
kinmari99 <kinga.wojtanowicz@microsoft.wsei.edu.pl>
2025-01-06 19:52:05 +0100

    Dodano logowanie

---
+++ Hotel/Controllers/LogowanieController.cs
@@ -0,0 +1,28 @@
+﻿using Hotel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Controllers
+{
+    public class LogowanieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Logowanie(Logowanie logowanie)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Wynik", logowanie);
+            }
+            else { return View("Index", logowanie); }
+        }
+        [HttpPost]
+        public IActionResult Wynik(Logowanie logowanie)
+        {
+            return View(logowanie);
+        }
+    }
+}
---
+++ Hotel/Models/Logowanie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotel.Models
+{
+    public class Logowanie
+    {
+        [Required(ErrorMessage = "Proszę podaj login")]
+        [MinLength(8)]
+        public string Nickname { get; set; }
+
+
+        [Required(ErrorMessage = "Proszę podaj Hasło.")]
+        [MinLength(8)]
+        [RegularExpression("((?=.*\\d).*(?=.*[a-z]).*(?=.*[A-Z]).*)")]
+        public string Haslo { get; set; }
+    }
+}
