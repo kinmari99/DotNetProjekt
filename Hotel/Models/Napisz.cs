c06fd2e3c7920b453f53815c3beed91f97e03fa5
kinmari99 <kinga.wojtanowicz@microsoft.wsei.edu.pl>
2025-01-06 18:15:12 +0100

    Dodano kolejny zwalidowany formularz

---
+++ Hotel/Models/Napisz.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotel.Models
+{
+    public class Napisz
+    {
+        [Required(ErrorMessage = "Proszę podaj Imię")]
+        [MinLength(2)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proszę podaj Nazwisko")]
+        [MinLength(2)]
+        public string Surname {  get; set; }
+
+
+        [Required(ErrorMessage = "Proszę podaj Email")]
+        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
+        public string Email { get; set; }
+
+        [Display(Name = "Wiadomość")]
+        [Required(ErrorMessage = "Wiadomość musi posiadać od 3 do 200 znaków.")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Wiadomość musi mieć od 3 do 200 znaków.")]
+        public string Message { get; set; }
+
+    }
+}
