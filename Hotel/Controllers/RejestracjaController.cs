8c0d4d0f960ebae0b1496aaa3454ffba16e6fe6a
kinmari99 <kinga.wojtanowicz@microsoft.wsei.edu.pl>
2025-01-06 19:44:10 +0100

    Dodano formularz rejestracji

---
+++ Hotel/Controllers/RejestracjaController.cs
@@ -0,0 +1,28 @@
+﻿using Hotel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Controllers
+{
+    public class RejestracjaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Rejestracja(Rejestracja rejestracja)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Wynik", rejestracja);
+            }
+            else { return View("Index", rejestracja); }
+        }
+        [HttpPost]
+        public IActionResult Wynik(Rejestracja rejestracja)
+        {
+            return View(rejestracja);
+        }
+    }
+}
---
+++ Hotel/Models/Rejestracja.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotel.Models
+{
+    public class Rejestracja
+    {
+        [Required(ErrorMessage = "Proszę podaj Imię")]
+        [MinLength(2)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proszę podaj Nazwisko")]
+        [MinLength(2)]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Proszę podaj login")]
+        [MinLength(8)]
+        public string Nickname { get; set; }
+
+        [Required(ErrorMessage = "Proszę podaj Email")]
+        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Proszę podaj Hasło. Minimum 8 znaków, 1 cyfra, 1 znak specjalny, 1 mała litera, 1 wielka.")]
+        [MinLength(8)]
+        [RegularExpression("((?=.*\\d).*(?=.*[a-z]).*(?=.*[A-Z]).*)")]
+        public string Haslo { get; set; }
+
+        [Required(ErrorMessage = "Proszę powtórz Hasło")]
+        [MinLength(8)]
+        [RegularExpression("((?=.*\\d).*(?=.*[a-z]).*(?=.*[A-Z]).*)")]
+        [Compare(nameof(Haslo), ErrorMessage = ("Hasła różnią się"))]
+        public string powtorzHaslo { get; set; }
+    }
+}
